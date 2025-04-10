7e05fb939741f6a4fb01834a8ad34147c603c204
ansharma-as <anshsharma712.as@gmail.com>
2025-04-10 10:17:50 +0530

    create api for backend and loading and not found page

---
+++ app/api/user/route.js
@@ -0,0 +1,6 @@
+export function GET(){
+    return Response.json({
+        "email": "ansh@gmail.com",
+        "name":"Ansh"
+    })
+}
---
+++ app/loading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function loading() {
+  return (
+    <div className='w-screen h-screen text-center mx-auto my-auto'>loading...</div>
+  )
+}
+
+export default loading
